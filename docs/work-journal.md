b10283981ad39f8eeb3299c5f5389aa7d4205f52
jacerobinson21@gmail.com <Jace Robinson>
2022-07-31 18:03:20 +0000

    got initial tutorial to run but no output due to render issues.

---
+++ docs/work-journal.md
@@ -0,0 +1,10 @@
+# Start Day 7-31-22
+
+Okay setup the project... Now to actually work...
+
+Okay having issues with the renderer, I think because I am in a docker container... 
+
+Next time...
+
+1. Go through tutorials here https://www.gymlibrary.ml/content/tutorials/
+2. Do I need to fix my render problem? Probably?
--- sample/main.py
+++ sample/main.py
@@ -1,4 +1,15 @@
-import numpy as np
-import gym as gym
+"""
+Sample file for learning openai gym
+"""
 
-print(f"hello world")
+import gym
+env = gym.make("LunarLander-v2", render_mode="rgb_array")
+env.action_space.seed(42)
+
+observation, info = env.reset(seed=42, return_info=True)
+
+for _ in range(1000):
+    observation, reward, done, info = env.step(env.action_space.sample())
+    if done:
+        observation, info = env.reset(return_info=True)
+env.close()
