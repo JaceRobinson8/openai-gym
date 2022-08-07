f7226d3fa955b984d751106571fba38755543b53
jacerobinson21@gmail.com <Jace Robinson>
2022-08-07 15:58:28 +0000

    commit before dropping dev container for this project.

--- docs/work-journal.md
+++ docs/work-journal.md
@@ -8,3 +8,32 @@ Next time...
 
 1. Go through tutorials here https://www.gymlibrary.ml/content/tutorials/
 2. Do I need to fix my render problem? Probably?
+
+Okay going through https://blog.paperspace.com/getting-started-with-openai-gym/.
+
+# Start Day 8-4-22
+
+Okay short programming session!
+
+So... specifically I had a problem with the video device... ideas...
+
+1. Just say screw it, do this outside of docker with poetry virtual env
+2. figure out this video device problem in docker...
+
+work 2 for a few minutes then say screw it... So why do I need a video device... I should just need to display a picture... Can I not display plots?
+
+Recall it is not just plots. I am actually like rendering and possibly running games? Using pygame which uses opengl and actual game rendering services.
+
+Okay admit defeat... Time to run without docker.
+
+
+
+# Start Day 8-7-22
+
+1. Fix the environment issues
+2. Get through the basic tutorial
+3. Read the paper
+
+### Notes on env issue
+
+Okay due to video device problems, just bail on docker for this project? Just use poetry 
--- sample/main.py
+++ sample/main.py
@@ -2,14 +2,29 @@
 Sample file for learning openai gym
 """
 
+import logging
+from rich.logging import RichHandler
+
 import gym
-env = gym.make("LunarLander-v2", render_mode="rgb_array")
-env.action_space.seed(42)
+import matplotlib.pyplot as plt
 
-observation, info = env.reset(seed=42, return_info=True)
+# create logger
+logging.basicConfig(
+    level=logging.INFO, format="%(message)s", handlers=[RichHandler()]
+)
+logger = logging.getLogger("gym_logger")
 
-for _ in range(1000):
-    observation, reward, done, info = env.step(env.action_space.sample())
-    if done:
-        observation, info = env.reset(return_info=True)
+env = gym.make('MountainCar-v0', new_step_api=False)
+obs_space = env.observation_space
+action_space = env.action_space
+logger.info("The observation space: %s", obs_space)
+logger.info("The action space: %s", action_space)
+ 
+obs = env.reset()
+logger.info("The initial observation is %s", obs)
+random_action = env.action_space.sample()
+new_obs, reward, done, info = env.step(random_action) # type: ignore
+logger.info("The new observation is %s", new_obs)
+
+env.render(mode = "human")
 env.close()
